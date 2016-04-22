f735e720f0360529f1a80bf4c936b18579f56fab
veyndan <veyndan89@gmail.com>
2016-04-22 22:48:48 +0100

    Set up image view

--- app/src/main/java/com/veyndan/katana/MainActivity.java
+++ app/src/main/java/com/veyndan/katana/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.profile) ImageView profile;
     @Bind(R.id.name) TextView name;
     @Bind(R.id.date) TextView date;
+    @Bind(R.id.image) ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        String imageUrl = "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12923224_20125624156346" +
-                "37_6490425404654077477_n.jpg?oh=8b2c57c4d2ec59ba0605d28fe68a1e5c&oe=57B59F67";
-        Glide.with(this).load(imageUrl).into(profile);
+        String profileUrl = "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12923224_2012562415634" +
+                "637_6490425404654077477_n.jpg?oh=8b2c57c4d2ec59ba0605d28fe68a1e5c&oe=57B59F67";
+        Glide.with(this).load(profileUrl).into(profile);
         name.setText("Veyndan Stuart");
         date.setText("1 Apr at 15:17");
+        String imageUrl = "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/t31.0-8/12916955_201" +
+                "6582558565956_3030515090458717633_o.jpg";
+        Glide.with(this).load(imageUrl).into(image);
     }
 }
