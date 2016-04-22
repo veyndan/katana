7caa2c1f506e3ff6c5602c61942baf32395a44ca
veyndan <veyndan89@gmail.com>
2016-04-22 21:55:56 +0100

    Set up name view

--- app/src/main/java/com/veyndan/katana/MainActivity.java
+++ app/src/main/java/com/veyndan/katana/MainActivity.java
@@ -3,6 +3,7 @@ package com.veyndan.katana;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -12,6 +13,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.profile) ImageView profile;
+    @Bind(R.id.name) TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
         String imageUrl = "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12923224_20125624156346" +
                 "37_6490425404654077477_n.jpg?oh=8b2c57c4d2ec59ba0605d28fe68a1e5c&oe=57B59F67";
         Glide.with(this).load(imageUrl).into(profile);
+        name.setText("Veyndan Stuart");
     }
 }
