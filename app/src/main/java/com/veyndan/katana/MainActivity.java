d7d338737d30e2087a104e073643f24e6e5aadfa
veyndan <veyndan89@gmail.com>
2016-04-23 14:26:12 +0100

    Added more dummy data

--- app/src/main/java/com/veyndan/katana/MainActivity.java
+++ app/src/main/java/com/veyndan/katana/MainActivity.java
@@ -36,16 +36,17 @@ public class MainActivity extends BaseActivity {
 
     private List<Post> init() {
         List<Post> posts = new ArrayList<>();
-        posts.add(new Post("https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12923224_2012562415634" +
-                "637_6490425404654077477_n.jpg?oh=8b2c57c4d2ec59ba0605d28fe68a1e5c&oe=57B59F67",
-                "Veyndan Stuart", "1 Apr at 15:17",
-                "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/t31.0-8/12916955_201" +
-                        "6582558565956_3030515090458717633_o.jpg"));
-        posts.add(new Post("https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12923224_2012562415634" +
-                "637_6490425404654077477_n.jpg?oh=8b2c57c4d2ec59ba0605d28fe68a1e5c&oe=57B59F67",
-                "Veyndan Stuart", "1 Apr at 15:17",
-                "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/t31.0-8/12916955_201" +
-                        "6582558565956_3030515090458717633_o.jpg"));
+
+        String profileUrl = "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/12923224_201256241563" +
+                "4637_6490425404654077477_n.jpg?oh=8b2c57c4d2ec59ba0605d28fe68a1e5c&oe=57B59F67";
+        String imageUrl = "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/t31.0-8/12916955_20" +
+                "16582558565956_3030515090458717633_o.jpg";
+        posts.add(new Post(profileUrl, "Veyndan Stuart", "1 Apr at 15:17", imageUrl));
+
+        profileUrl = "https://images.unsplash.com/photo-1456769355437-50afa71c8863?ixlib=rb-0.3." +
+                "5&q=80&fm=jpg&crop=entropy&s=c9119822a43b8fbf134705964b90148c";
+        imageUrl = profileUrl;
+        posts.add(new Post(profileUrl, "Roksolana Zasiadko updated her profile picture.", "28 Mar at 12:54", imageUrl));
         return posts;
     }
 }
