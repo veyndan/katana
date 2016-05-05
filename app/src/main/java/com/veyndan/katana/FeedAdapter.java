0544265ac1a016f8834b9c78369799b00535a2aa
veyndan <veyndan89@gmail.com>
2016-05-05 02:23:09 +0100

    Move post initialization to adapter

--- app/src/main/java/com/veyndan/katana/FeedAdapter.java
+++ app/src/main/java/com/veyndan/katana/FeedAdapter.java
@@ -8,13 +8,36 @@ import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FeedAdapter extends RecyclerView.Adapter<VH> {
 
     private final List<Post> posts;
 
-    public FeedAdapter(List<Post> posts) {
+    public FeedAdapter(Context context, Feed feed) {
+        List<Post> posts = new ArrayList<>();
+
+        String profileUrl, text;
+
+        Feed.Data data = feed.data[0];
+
+        profileUrl = FacebookAPI.get(context, FacebookAPI.RequestBuilder.picture(data.from.id), Picture.class).data.url;
+        Post.Description description = new Post.Description("Couldn't be happier.", data.picture);
+        posts.add(new Post(context, profileUrl, data.from.name, Post.NONE, data.created_time, description));
+
+        profileUrl = "https://images.unsplash.com/photo-1456769355437-50afa71c8863?ixlib=rb-0.3." +
+                "5&q=80&fm=jpg&crop=entropy&s=c9119822a43b8fbf134705964b90148c";
+        description = new Post.Description(null, profileUrl);
+        posts.add(new Post(context, profileUrl, "Roksolana Zasiadko", Post.UPDATE_PROFILE_PICTURE, "28 Mar at 12:54", description));
+
+        profileUrl = "https://images.unsplash.com/photo-1460752652228-71887ef91aa4?ixlib=rb-0.3." +
+                "5&q=80&fm=jpg&crop=entropy&s=091cb2288d1e101559437c487058edd1";
+        text = "Calm down mechanic guy. Just here for an oil change. If I wanted to know about " +
+                "all the other shit wrong with my car I'd turn the radio down.";
+        description = new Post.Description(text, null);
+        posts.add(new Post(context, profileUrl, "Sergey Svechnikov", Post.NONE, "27 Mar at 22:03", description));
+
         this.posts = posts;
     }
 
--- app/src/main/java/com/veyndan/katana/MainActivity.java
+++ app/src/main/java/com/veyndan/katana/MainActivity.java
@@ -11,9 +11,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -43,8 +40,10 @@ public class MainActivity extends BaseActivity {
             ab.setTitle(null);
         }
 
+        Feed feed = FacebookAPI.get(this, FacebookAPI.RequestBuilder.feed(FacebookAPI.ID_ME), Feed.class);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new FeedAdapter(init()));
+        recyclerView.setAdapter(new FeedAdapter(this, feed));
         recyclerView.setItemAnimator(new DefaultItemAnimator() {
             @Override
             public boolean canReuseUpdatedViewHolder(@NonNull RecyclerView.ViewHolder viewHolder) {
@@ -53,35 +52,6 @@ public class MainActivity extends BaseActivity {
                 return true;
             }
         });
-    }
-
-    private List<Post> init() {
-        List<Post> posts = new ArrayList<>();
-
-        String profileUrl, text;
-
-        Feed feed = FacebookAPI.get(this, FacebookAPI.RequestBuilder.feed(FacebookAPI.ID_ME), Feed.class);
-
-        Feed.Data data = feed.data[0];
-
-        profileUrl = FacebookAPI.get(this, FacebookAPI.RequestBuilder.picture(data.from.id), Picture.class).data.url;
-        Post.Description description = new Post.Description("Couldn't be happier.", data.picture);
-        posts.add(new Post(this, profileUrl, data.from.name, Post.NONE, data.created_time, description));
-
-        profileUrl = "https://images.unsplash.com/photo-1456769355437-50afa71c8863?ixlib=rb-0.3." +
-                "5&q=80&fm=jpg&crop=entropy&s=c9119822a43b8fbf134705964b90148c";
-        description = new Post.Description(null, profileUrl);
-        posts.add(new Post(this, profileUrl, "Roksolana Zasiadko", Post.UPDATE_PROFILE_PICTURE, "28 Mar at 12:54", description));
-
-        profileUrl = "https://images.unsplash.com/photo-1460752652228-71887ef91aa4?ixlib=rb-0.3." +
-                "5&q=80&fm=jpg&crop=entropy&s=091cb2288d1e101559437c487058edd1";
-        text = "Calm down mechanic guy. Just here for an oil change. If I wanted to know about " +
-                "all the other shit wrong with my car I'd turn the radio down.";
-        description = new Post.Description(text, null);
-        posts.add(new Post(this, profileUrl, "Sergey Svechnikov", Post.NONE, "27 Mar at 22:03", description));
-
-
-        return posts;
     }
 
     @Override
